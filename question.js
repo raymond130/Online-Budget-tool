d2112c07fdf988fe6d3d3fba8b63dc63f1b23bb8
raymond130 <45745238+raymond130@users.noreply.github.com>
2021-11-17 17:42:37 -0500

    Update question.js

--- question.js
+++ question.js
@@ -1,35 +1,35 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Budget from 'Budget'
 
-const[attribute, setAttribute] = useState({})
+const [attribute, setAttribute] = useState({})
 
 const handleInput = (event) => {
         const value = event.target.value;
-        
+
         setAttribute({...attribute,
-        [event.target.question: value]}
-}
+            [event.target.question]: value
+        })
 
 
-const addToBudget = (event,budgetQues, value) => {
+        const addToBudget = (event, budgetQues, value) => {
 
-        event.preventDefault()
-        /* line below should map the dictionary of attributes to budget attributes*/
-        attriute.map()
-        
-}
-
-const Question = ({ question }) => {
-
-        return ( <label>
-                {question}
-                <input 
-                        type = "text"
-                        question = {question}
-                        value = {attribute.question}
-                        onChange = {handleInput}
-                />     
-        
-         </label>)
+            event.preventDefault()
+                /* line below should map the dictionary of attributes to budget attributes*/
+            Object.entries(attribute).map(([key, value]) => Budget.budget)
 
         }
+
+        const Question = ({ question }) => {
+
+                return ( < label > { question } <
+                    input type = "text"
+                    question = { question }
+                    value = { attribute.question }
+                    onChange = { handleInput }
+                    />     
+
+
+                    <
+                    /label>)
+
+                }
