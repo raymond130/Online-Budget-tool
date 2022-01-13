27204cee181a6d0f8148b1204f43860e27d942e5
raymond130 <45745238+raymond130@users.noreply.github.com>
2022-01-12 22:29:52 -0500

    Add files via upload

---
+++ displayBudget.js
@@ -0,0 +1,30 @@
+import Budget from "./Budget.js"
+import questions from "./questionsList.js"
+
+//first thing is to get the budget prop
+
+
+thisBudget = {}
+
+const budgetToDisplay = (props) => {
+    thisBudget = props;
+}
+
+
+ const displayBudget = () => {
+
+    return (
+
+        <ol>
+            <li>Your total income is: {thisBudget.returnIncome}</li>
+            <li>Your total expenses are: {thisBudget.returnExpenses}</li>
+            <li>Your total spending money is: {thisBudget.freeSpend}</li>
+
+
+        </ol>
+
+
+    )  
+ }
+
+ export default displayBudget
