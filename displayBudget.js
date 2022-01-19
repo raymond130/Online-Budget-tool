1f7926b4dee84aaff18dfd7eaf974ef04866040c
raymond130 <45745238+raymond130@users.noreply.github.com>
2022-01-18 22:47:56 -0500

    Update displayBudget.js

--- displayBudget.js
+++ displayBudget.js
@@ -4,14 +4,15 @@ import questions from "./questionsList.js"
 //first thing is to get the budget prop
 
 
-thisBudget = {}
+var thisBudget = {}
 
-const budgetToDisplay = (props) => {
+export const budgetToDisplay = (props) => {
     thisBudget = props;
+    displayBudget();
 }
 
 
- const displayBudget = () => {
+ export const displayBudget = () => {
 
     return (
 
@@ -19,12 +20,8 @@ const budgetToDisplay = (props) => {
             <li>Your total income is: {thisBudget.returnIncome}</li>
             <li>Your total expenses are: {thisBudget.returnExpenses}</li>
             <li>Your total spending money is: {thisBudget.freeSpend}</li>
-
-
         </ol>
 
 
     )  
  }
-
- export default displayBudget
